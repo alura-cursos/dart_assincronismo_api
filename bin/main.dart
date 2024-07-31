cb4e56759351e0331033f437bf1a0f09d3af0ba5
Ricarth Lima <ricarth.lima@gmail.com>
2024-07-31 15:41:43 -0300

    v1.4 - fazendo o primeiro assincronismo

--- bin/main.dart
+++ bin/main.dart
@@ -8,5 +8,6 @@ void main() {
 requestData() {
   String url =
       "https://gist.githubusercontent.com/ricarthlima/a0eb198cb7a70696c4031e7e577de0cd/raw/356ce2c39dfd58d3d2e948d1ad87ea828544f1db/accounts.json";
-  print(get(Uri.parse(url)));
+  Future<Response> futureResponse = get(Uri.parse(url));
+  print(futureResponse);
 }
