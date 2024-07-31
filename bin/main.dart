3023c35da3059b167f3abe3efe3cdd24269df57f
Ricarth Lima <ricarth.lima@gmail.com>
2024-07-31 15:42:11 -0300

    v1.5 - entendendo o future

--- bin/main.dart
+++ bin/main.dart
@@ -10,4 +10,10 @@ requestData() {
       "https://gist.githubusercontent.com/ricarthlima/a0eb198cb7a70696c4031e7e577de0cd/raw/356ce2c39dfd58d3d2e948d1ad87ea828544f1db/accounts.json";
   Future<Response> futureResponse = get(Uri.parse(url));
   print(futureResponse);
+  futureResponse.then(
+    (Response response) {
+      print(response);
+      print(response.body);
+    },
+  );
 }
