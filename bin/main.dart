e72821afa78813e050044556634618cdfcf3f043
ricarthlima <rrsl@cin.ufpe.br>
2024-08-01 16:21:49 -0300

    v4.3 - enviando dados para a stream

--- bin/main.dart
+++ bin/main.dart
@@ -13,40 +13,33 @@ void main() {
     },
   );
 
-  //requestData();
-  //requestDataAsync();
-  // sendDataAsync({
-  //   "id": "NEW001",
-  //   "name": "Flutter",
-  //   "lastName": "Dart",
-  //   "balance": 5000,
-  // });
+  requestData();
+  requestDataAsync();
+  sendDataAsync({
+    "id": "NEW001",
+    "name": "Flutter",
+    "lastName": "Dart",
+    "balance": 5000,
+  });
 }
 
 requestData() {
   String url =
       "https://gist.githubusercontent.com/ricarthlima/a0eb198cb7a70696c4031e7e577de0cd/raw/356ce2c39dfd58d3d2e948d1ad87ea828544f1db/accounts.json";
   Future<Response> futureResponse = get(Uri.parse(url));
-  print(futureResponse);
   futureResponse.then(
     (Response response) {
-      print(response);
-      print(response.body);
-      List<dynamic> listAccounts = json.decode(response.body);
-      Map<String, dynamic> mapCarla = listAccounts.firstWhere(
-        (element) => element["name"] == "Carla",
-      );
-      print(mapCarla["balance"]);
+      streamController
+          .add("${DateTime.now()} | Requisição de leitura (usando then).");
     },
   );
-
-  print("Última coisa a acontecer na função.");
 }
 
 Future<List<dynamic>> requestDataAsync() async {
   String url =
       "https://gist.githubusercontent.com/ricarthlima/a0eb198cb7a70696c4031e7e577de0cd/raw/356ce2c39dfd58d3d2e948d1ad87ea828544f1db/accounts.json";
   Response response = await get(Uri.parse(url));
+  streamController.add("${DateTime.now()} | Requisição de leitura.");
   return json.decode(response.body);
 }
 
@@ -70,5 +63,12 @@ sendDataAsync(Map<String, dynamic> mapAccount) async {
       }
     }),
   );
-  print(response.statusCode);
+
+  if (response.statusCode.toString()[0] == "2") {
+    streamController.add(
+        "${DateTime.now()} | Requisição adição bem sucedida (${mapAccount["name"]}).");
+  } else {
+    streamController
+        .add("${DateTime.now()} | Requisição falhou (${mapAccount["name"]}).");
+  }
 }
